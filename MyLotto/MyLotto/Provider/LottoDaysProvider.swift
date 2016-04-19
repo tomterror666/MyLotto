ea82df892f95d49b889931471e60dbb7b23452a4
Andre Heß <hess@myhammer.de>
2016-04-19 22:54:16 +0200

    changed: generate list of all drawing dates since given date - without GET requests still...

--- MyLotto/MyLotto/Provider/LottoDaysProvider.swift
+++ MyLotto/MyLotto/Provider/LottoDaysProvider.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-typealias LottoDaysCompletion = (Void) -> (NSArray)
+typealias LottoDaysCompletion = (NSArray) -> (Void)
 
 class LottoDaysProvider: NSObject {
 	
 	var allLottoDays:NSMutableDictionary
 	var httpManager:HTTPManager
+	var operationQueue:NSOperationQueue
 	
 	static func sharedProvider() -> LottoDaysProvider {
 		let me = LottoDaysProvider()
@@ -23,12 +24,50 @@ class LottoDaysProvider: NSObject {
 	override init() {
 		self.allLottoDays = NSMutableDictionary()
 		self.httpManager = HTTPManager.sharedManager()
+		self.operationQueue = NSOperationQueue()
+		self.operationQueue.maxConcurrentOperationCount = 1
 		super.init()
 	}
 	
-	func loadLottoDaysSinceDate(date:NSDate, completion:LottoDaysCompletion) {
-		//todo: gehe liste der jahre durch und wenn noch nicht geladen, dann pack laderequest auf nsoperationqueue
-		// sind alle laderequests fertig (operationaueue ist leer) oder wurden alle jahre gefunden, dann gehe diese liste nochmals durch und bilde result
+	func loadLottoDaysSinceDate(date:NSDate, completion:LottoDaysCompletion?) {
+		let startingYear = self.getYearFromDate(date)
+		let endingYear = self.getYearFromDate(NSDate())
+		for countingYear in startingYear...endingYear {
+			if self.allLottoDays.objectForKey("\(countingYear)") == nil {
+				let loadingOP:NSBlockOperation = NSBlockOperation.init(block: {
+					//todo: gehe liste der jahre durch und wenn noch nicht geladen, dann pack laderequest auf nsoperationqueue
+					// sind alle laderequests fertig (operationaueue ist leer) oder wurden alle jahre gefunden, dann gehe diese liste nochmals durch und bilde result
+				})
+				self.operationQueue.addOperation(loadingOP)
+			}
+		}
+		if (self.operationQueue.operationCount == 0) {
+			if (completion != nil) {
+				completion!(self.collectAllLottoDaysSinceDate(date))
+			}
+		}
+	}
+	
+	func collectAllLottoDaysSinceDate(date:NSDate) -> NSArray {
+		let result = NSMutableArray()
+		for year in self.allLottoDays.allKeys {
+			let dateString = self.allLottoDays.objectForKey(year)
+			let lottoDate = self.dateFromString(dateString as! String)
+			result.addObject(lottoDate)
+		}
+		return result
+	}
+	
+	func dateFromString(dateString:String) -> NSDate {
+		let formatter:NSDateFormatter = NSDateFormatter()
+		formatter.dateFormat = "yyyy-MM-dd"
+		return formatter.dateFromString(dateString)!
+	}
+	
+	func getYearFromDate(date:NSDate) -> Int {
+		let calendar:NSCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+		let dateComponetns:NSDateComponents = calendar.components(NSCalendarUnit.Year, fromDate: date)
+		return dateComponetns.year
 	}
 
 }
