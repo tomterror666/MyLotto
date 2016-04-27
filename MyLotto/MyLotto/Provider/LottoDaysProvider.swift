4b9b0c013b86a5a7104b43da7b925aa0593dc3f4
Andre Heß <andre.hess@myhammer.de>
2016-04-27 14:29:45 +0200

    changed: make app loading all lotto days since given date in array

--- MyLotto/MyLotto/Provider/LottoDaysProvider.swift
+++ MyLotto/MyLotto/Provider/LottoDaysProvider.swift
@@ -15,6 +15,8 @@ class LottoDaysProvider: NSObject {
 	var allLottoDays:NSMutableDictionary
 	var httpManager:HTTPManager
 	var operationQueue:NSOperationQueue
+	var loadLottoDaysCompletion:LottoDaysCompletion?
+	var loadLottoDaysStartDate:NSDate!
 	
 	static func sharedProvider() -> LottoDaysProvider {
 		let me = LottoDaysProvider()
@@ -26,14 +28,19 @@ class LottoDaysProvider: NSObject {
 		self.httpManager = HTTPManager.sharedManager()
 		self.operationQueue = NSOperationQueue()
 		self.operationQueue.maxConcurrentOperationCount = 1
+		self.operationQueue.qualityOfService = NSQualityOfService.Background
 		super.init()
 	}
 	
 	func loadLottoDaysSinceDate(date:NSDate, completion:LottoDaysCompletion?) {
 		let startingYear = self.getYearFromDate(date)
 		let endingYear = self.getYearFromDate(NSDate())
+		self.loadLottoDaysCompletion = completion
+		self.loadLottoDaysStartDate = date
+		var numberOfYears = endingYear - startingYear
 		for countingYear in startingYear...endingYear {
 			if self.allLottoDays.objectForKey("\(countingYear)") == nil {
+				
 				let loadingOP:NSBlockOperation = NSBlockOperation.init(block: {
 					//todo: gehe liste der jahre durch und wenn noch nicht geladen, dann pack laderequest auf nsoperationqueue
 					// sind alle laderequests fertig (operationaueue ist leer) oder wurden alle jahre gefunden, dann gehe diese liste nochmals durch und bilde result
@@ -42,16 +49,18 @@ class LottoDaysProvider: NSObject {
 						if let requestError = error {
 							print("\(requestError)")
 						} else {
-							let requestResponse = requestObject as! NSDictionary
-							if let lottodaysInResonse = requestResponse.objectForKey(countingYear) {
-								self.allLottoDays.setObject(lottodaysInResonse, forKey: countingYear)
-							}
-							print("Request to \(requestUrlString) finished with folloning \(self.operationQueue.operationCount) requests")
-							if (self.operationQueue.operationCount == 1) {
-								if (completion != nil) {
-									completion!(self.collectAllLottoDaysSinceDate(date))
+						//	dispatch_async(dispatch_get_main_queue(), { 
+								let requestResponse = requestObject as! NSDictionary
+								if let lottodaysInResonse = requestResponse.objectForKey("\(countingYear)") {
+									self.allLottoDays.setObject(lottodaysInResonse, forKey: "\(countingYear)")
 								}
-							}
+								print("Request to \(requestUrlString) finished with folloning \(numberOfYears) requests")
+								if (numberOfYears-- == 0) {
+									if (completion != nil) {
+										completion!(self.collectAllLottoDaysSinceDate(date))
+									}
+								}
+						//	})
 						}
 					})
 				})
@@ -62,15 +71,22 @@ class LottoDaysProvider: NSObject {
 			if (completion != nil) {
 				completion!(self.collectAllLottoDaysSinceDate(date))
 			}
+		} else {
+//			self.operationQueue.addObserver(self, 
+//			                                forKeyPath: "operationCount", 
+//			                                options: NSKeyValueObservingOptions.New, 
+//			                                context: nil)
 		}
 	}
 	
 	func collectAllLottoDaysSinceDate(date:NSDate) -> NSArray {
 		let result = NSMutableArray()
 		for year in self.allLottoDays.allKeys {
-			let dateString = self.allLottoDays.objectForKey(year)
-			let lottoDate = self.dateFromString(dateString as! String)
-			result.addObject(lottoDate)
+			let dateArray:[NSDictionary] = self.allLottoDays.objectForKey(year) as! [NSDictionary]
+			for dateDict:NSDictionary in dateArray {
+				let lottoDate = self.dateFromString(dateDict.objectForKey("date") as! String)
+				result.addObject(lottoDate)
+			}
 		}
 		return result
 	}
@@ -92,4 +108,14 @@ class LottoDaysProvider: NSObject {
 		return dateComponetns.year
 	}
 
+	
+	
+	override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+		//print("Request finished with folloning \(self.operationQueue.operationCount) requests")
+		if (self.operationQueue.operationCount == 1) {
+			if (self.loadLottoDaysCompletion != nil) {
+				self.loadLottoDaysCompletion!(self.collectAllLottoDaysSinceDate(self.loadLottoDaysStartDate))
+			}
+		}
+	}
 }
